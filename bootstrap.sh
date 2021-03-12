e5bcc2b6eea4af11de46719c3aaceba6d988d76b
Adina Wagner <adina.wagner@t-online.de>
2021-03-12 15:29:01 +0100

    Fix git remote url

--- bootstrap.sh
+++ bootstrap.sh
@@ -76,7 +76,7 @@ git commit --no-edit --amend --author "Felix Hoffstaedter <f.hoffstaedter@fz-jue
 # output sibling, and the analysis will start with a clone from the input
 # sibling.
 datalad create-sibling-ria -s output "${output_store}"
-pushremote=$(git remote get-url --push joc)
+pushremote=$(git remote get-url --push output)
 datalad create-sibling-ria -s input --storage-sibling off "${input_store}"
 
 # register the input dataset
