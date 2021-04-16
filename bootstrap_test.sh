1b28ba9f76914aecc801b854a2853b8f175099b5
Adina Wagner <adina.wagner@t-online.de>
2021-04-16 08:54:52 +0200

    Write runfmriprep.sh as part of the job setup in a heredoc
    This reduces the clutter in the repository

--- bootstrap_test.sh
+++ bootstrap_test.sh
@@ -70,8 +70,6 @@ git commit --amend -m 'Register pipeline dataset'
 # operations below. (The scripts below are only relevant for CAT processing)
 cp ~/license.txt code/license.txt
 datalad save -m "Add Freesurfer license file"
-cp ../runfmriprep.sh code/runfmriprep.sh
-datalad save -m "Add fmriprep job script"
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
@@ -85,6 +83,47 @@ datalad create-sibling-ria -s input --storage-sibling off "${input_store}"
 datalad clone -d . ${data} inputs/data
 # amend the previous commit with a nicer commit message
 git commit --amend -m 'Register input data dataset as a subdataset'
+
+
+# -----------------------------------------------------------------------------
+# FIXME: Customize your fmriprep call below.
+# -----------------------------------------------------------------------------
+# write a custom fmriprep call into a separate script. This allows to not only
+# specify an fmriprep call of your choice, but also perform necessary
+# preparations. In case of fmriprep, this is the creation of a temporary working
+# directory, and a temporary removal of all irrelevant JSON files, which ensures
+# recomputation.
+cat > code/runfmriprep.sh << "EOT"
+#!/bin/bash
+
+subid=$1
+
+# -----------------------------------------------------------------------------
+# create workdir for fmriprep inside the dataset to simplify singularity call
+# PWD will be available in the container
+mkdir -p .git/tmp/wdir
+
+# pybids (inside fmriprep) will try to read all JSON files in a dataset. In case
+# of a recomputation, JSON files of other subjects can be dangling symlinks.
+# We prevent pybids from crashing the fmriprep run when it can't read those, by
+# wiping them out temporarily via renaming.
+# We spare only those that belong to the participant we want to process.
+# After job completion, the jsons will be restored.
+# See https://github.com/bids-standard/pybids/issues/631 for more information.
+
+find inputs/data -mindepth 2 -name '*.json' -a ! -wholename "$subid" | sed -e "p;s/json/xyz/" | xargs -n2 mv
+
+# execute fmriprep. Its runscript is available as /singularity within the
+# container. Custom fmriprep parametrization can be done here.
+/singularity inputs/data . participant --participant-label $subid \
+    --anat-only -w .git/tmp/wdir --fs-no-reconall --skip-bids-validation \
+    --fs-license-file code/license.txt
+
+
+# restore the jsons we have moved out of the way
+find inputs/data -mindepth 2 -name '*.xyz' -a ! -wholename "$subid" | sed -e "p;s/xyz/json/" | xargs -n2 mv
+
+EOT
 
 
 # the actual compute job specification
